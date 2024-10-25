35f0a72934ec3b423539ee0ef2e167007cce50e1
Ankita Shendge <ankitashendge15@gmail.com>
2024-10-24 21:40:17 -0700

    redirect url changed

--- src/components/Authentication/sportify.js
+++ src/components/Authentication/sportify.js
@@ -1,6 +1,5 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-export const redirectUri =
-  "https://react-project-ankita-shendges-projects.vercel.app/";
+export const redirectUri = "ankitashendge-ankita-shendges-projects.vercel.app";
 // export const redirectUri = "http://localhost:3000/";
 //changed
 const clientId = "65f331ac024346659850288cc526fb16";
